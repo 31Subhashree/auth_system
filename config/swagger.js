2ec13cedc67f6e623a4154c8eab3ebb4d6e9b829
31Subhashree <48314407+31Subhashree@users.noreply.github.com>
2024-05-25 00:11:06 +0530

    feat: swagger.js file added

---
+++ config/swagger.js
@@ -0,0 +1,27 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Auth System API',
+            version: '1.0.0',
+            description: 'API documentation for the Authentication System',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'], // Path to the API docs
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const setupSwagger = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+module.exports = setupSwagger;
